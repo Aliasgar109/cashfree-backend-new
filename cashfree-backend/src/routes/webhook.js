c561426698e4acef7cac8a03052ef181dc9c8c38
Aliasgar109 <125254104+Aliasgar109@users.noreply.github.com>
2025-08-26 11:23:09 +0530

    Remove node_modules from repo

--- cashfree-backend/src/routes/webhook.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const webhookController = require('../controllers/webhookController');
-
-// Handle Cashfree webhook
-router.post('/cashfree', webhookController.handleCashfreeWebhook);
-
-module.exports = router;
